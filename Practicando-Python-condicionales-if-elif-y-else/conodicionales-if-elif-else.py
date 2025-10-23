7b789cff0172ece175f118c8e0645d729b06a255
andresfllanten <andresfllanten@gmail.com>
2025-10-22 21:49:20 -0500

    Solucion ejercicios de condicionales

---
+++ Practicando-Python-condicionales-if-elif-y-else/conodicionales-if-elif-else.py
@@ -0,0 +1,11 @@
+"""
+Bruno gestiona un pequeño comercio y quiere saber qué producto tuvo el mejor
+ desempeño de ventas el mes pasado. Registró la cantidad vendida de dos 
+ productos: manzanas y plátanos. Ahora, necesita escribir un programa que 
+ identifique y muestre cuál de ellos tuvo más ventas.
+
+Crea un programa que reciba el número de ventas de los dos productos y muestre
+ un mensaje indicando cuál de ellos vendió más. Si las cantidades son iguales, 
+ muestra un mensaje diciendo que hubo un empate.
+
+"""
